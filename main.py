5db955cf7ec2f010fd6ed765ad09fb242f2d302a
alex <alex.olivier.2502@gmail.com>
2024-05-27 15:01:28 +0200

    add send_mail to test

--- main.py
+++ main.py
@@ -66,7 +66,81 @@ def analyseMsg(body: str):
         return True
 
 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send_email(subject, alert, sender, receiver, role="Responsable"):
+    msg = MIMEMultipart('alternative')
+
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receiver
+
+    # Create the HTML version of your message
+    html1 = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <title>Notification IMS Promo</title>
+    """
+    css1 = """
+    <style>
+        body { font-family: Arial, sans-serif; line-height: 1.6; }
+        .header { background: #f0f0f0; padding: 10px; text-align: center; }
+        .content { margin: 20px; }
+        .footer { background: #f0f0f0; padding: 10px; text-align: center; }
+        img { max-width: 100%; }
+    </style>
+    """
+    html2 = f"""
+    </head>
+    <body>
+        <div class="header">
+            <h1>IMS Promo Alert System</h1>
+        </div>
+        <div class="content">
+            <h2>Alerte importante : {alert}</h2>
+            <p>Cher/Chère {role},</p>
+            <p>Une alerte importante a été détectée dans notre application. Veuillez vérifier immédiatement pour prendre les mesures nécessaires.</p>
+            <p>Voici un aperçu de l'alerte :</p>
+            <p>{alert}</p>
+            <p>Pour plus de détails, accédez à l'application IMS Promo.</p>
+        </div>
+        <div class="footer">
+            <p>Merci de votre attention,</p>
+            <p>L'équipe IMS Promo</p>
+        </div>
+    </body>
+    </html>
+    """
+
+    html_part = MIMEText(html1 + css1 + html2, 'html')
+
+    msg.attach(html_part)
+    
+    server_name = "smtp.office365.com"
+    server_port = 587
+    # Connect to the SMTP server
+    s = smtplib.SMTP(server_name, server_port, local_hostname='localhost')
+    s.starttls()
+
+    # Login to the SMTP server
+    password = "Rpwnwqy#4250216!"
+    s.login(sender, password)
+    s.set_debuglevel(1)
+
+    # Send the email
+    s.send_message(msg)
+    print("Email sent successfully")
+    s.quit()
+    
+
+
 if __name__ == "__main__":
+    
+    # for _ in range(3):
+    #     send_email("test", "test", "alex.olivier@isen.yncrea.fr", "marc.etavard@isen.yncrea.fr")    
     events = urllib_to_json(urllib.request.urlopen("http://localhost:5000/event/getAll"))
     for e in events:
         checkMsg(f"[Event]{e}")
